253df310de48782c89710aa0ee4d18ba1fcd9f8a
Tom Catling <tom.catling@cma.gov.uk>
2019-02-27 09:55:29 +0000

    broke out logins to separate file

--- minimal_authenticator.py
+++ minimal_authenticator.py
@@ -17,13 +17,10 @@ import hashlib
 import getpass
 import os
 
+import toml
+
 from tornado import gen
 from jupyterhub.auth import Authenticator
-
-LOGINS = {
-    'admin': 'a19a489885c1b94cde75644e24937b84db532e7a2d6e74a91b55021c8974d3ccc72d144fd04e01ca18223c29909b0e378362a094354a6099c6fe1313431c1334191f14b7eb09b484a5663327fa8ac5c0130fb83ffb22f3023c38465d7c9bdc21',
-    'user' : '51b12c34b72d36f5f894076f644b56f6666f8854faebabd2394731544876200e247931ba9da23c51753ca9d28456a878fee2143eccc84fae5588fbeb324561d1ecf6d5412ca27f459346157c7159a710de4b5d18569266315e90a3b1b5d707b6'
-}
 
 def hash_password(password):
     """Hash a password for storing."""
@@ -48,9 +45,13 @@ def verify_password(stored_password, provided_password):
 
 class MinimalAuthenticator(Authenticator):
 
+    def __init__(self, logins_path='/etc/jupyterhub/logins.toml'):
+      with open(logins_path) as f:
+        self.logins = toml.loads(f.read())
+
     @gen.coroutine
     def authenticate(self, handler, data):
-        if verify_password(LOGINS.get(data['username']),data['password']):
+        if verify_password(self.logins.get(data['username']),data['password']):
             return data['username']
 
 if __name__ == '__main__':
